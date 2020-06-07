414ae1e58eb8aaf970f8b464ad62986a46d679fb
cookie-nini <1394956763@qq.com>
2020-06-07 12:40:55 +0800

    feat: cancel link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,12 +5,12 @@ import './index.styl'
 
 export default function Footer() {
     return <div className='footer'>
-        <a
+        <span
             target="_blank"
             href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=31011002005120">
             <img src={imgUrl}/>
             <p>沪公网安备 31011002005120号</p>
-        </a>
+        </span>
     </div>
 }
 
